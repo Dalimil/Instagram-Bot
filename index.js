cbb72fc88c8ba3b4793930792f59bfe56148431e
Dalimil <dalimilhajek@gmail.com>
2018-08-26 04:19:06 +0100

    Update basic index flow

--- index.js
+++ index.js
@@ -1,40 +1,15 @@
+const Url = require('./src/Url');
+const Auth = require('./src/Auth');
+const fs = require('fs');
 
-const Url = {
-  loginUrl: 'https://www.instagram.com/accounts/login/ajax/',
-  logoutUrl: 'https://www.instagram.com/accounts/logout/',
+const credentials = JSON.parse(fs.readFileSync('creds.json'));
 
-  getHashtagUrl(hashtag) =>
-    `https://www.instagram.com/explore/tags/${hashtag}/?__a=1`,
+Auth.login(credentials);
 
-  getLocationUrl(location) =>
-    `https://www.instagram.com/explore/locations/${location}/?__a=1`,
-
-  getUsernameUrl(username) =>
-    `https://www.instagram.com/${username}/?__a=1`,
-
-  getUserDetailUrl(username) =>
-   `https://i.instagram.com/api/v1/users/${username}/info/`,
-   // alternatively: https://www.instagram.com/${username}/
-
-  getFollowUrl(username) =>
-    `https://www.instagram.com/web/friendships/${username}/follow/`,
-
-  getUnfollowUrl(username) =>
-    `https://www.instagram.com/web/friendships/${username}/unfollow/`,
-
-  getLikeUrl(mediaId) =>
-    `https://www.instagram.com/web/likes/${mediaId}/like/`,
-
-  getUnlikeUrl(mediaId) =>
-    `https://www.instagram.com/web/likes/${mediaId}/unlike/`,
-
-  getMediaDetailUrl(mediaId) =>
-   `https://www.instagram.com/p/${mediaId}/?__a=1`,
-};
-
+// Auth.logout();
 
 // HTTP Error 400 = Instagram ban -> wait 2 hours
-const Http = {
+/*const Http = {
   // handle cookies etc. here
   get(url) => {
     // todo
@@ -42,7 +17,7 @@ const Http = {
   post(url, data) => {
     // todo
   }
-}
+}*/
 
 /*
 describe('Hangouts invites', function() {
