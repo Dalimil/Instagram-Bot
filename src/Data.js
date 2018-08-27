49343c2d001faaf50f0d53fe29c7139376a170f8
Dalimil <dalimilhajek@gmail.com>
2018-08-27 12:12:33 +0100

    Create backbone for persistent storage and retrieval

---
+++ src/Data.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+
+const Data = {
+  futureUnfollowListFile: './data/future-unfollow.json',
+  initialTargetsFile: './data/initial-targets.json',
+  processedAccountsFile: './data/processed-accounts.json',
+
+  getFutureUnfollowList() {
+    return JSON.parse(fs.readFileSync(Data.futureUnfollowListFile)).future_unfollow;
+  },
+
+  storeFutureUnfollowList(data) {
+    fs.writeFileSync(Data.futureUnfollowListFile, JSON.stringify({ future_unfollow: data }, null, 2));
+  },
+
+  getInitialTargets() {
+    return JSON.parse(fs.readFileSync(Data.initialTargetsFile));
+  },
+
+  getProcessedAccountsList() {
+    return JSON.parse(fs.readFileSync(Data.processedAccountsFile)).processed_accounts;
+  },
+
+  storeProcessedAccountsList(data) {
+    fs.writeFileSync(Data.processedAccountsFile, JSON.stringify({ processed_accounts: data }, null, 2));
+  },
+};
+
+module.exports = Data;
