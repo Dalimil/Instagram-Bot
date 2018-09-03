baef6ee24b98a2023c5211b6eb61a88e5f5c53d1
Dalimil <dalimilhajek@gmail.com>
2018-09-03 17:06:16 +0100

    Increase algorithm unfollow limits notes

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const TerminalBot = require('./src/terminal');
 (async () => {
   // Follow limit: between 100 up to max 500 per day
   // HARD LIMIT: 40 follows per hour!
-  // Unknown unfollow limit: but 70 per hour went without problems
+  // Unknown unfollow limit: but 120 per hour ran without problems
   // 7500 total following is the global MAX
   // Max number of likes is 1.5x that amount
 
