e0c84c4df5e5046a7ec555d1d97e14edde387b44
Dalimil <dalimilhajek@gmail.com>
2019-02-20 16:39:26 -0800

    Update start up script

--- vm-start-script.sh
+++ vm-start-script.sh
@@ -2,9 +2,13 @@
 
 pkill -9 java
 pkill -9 chromium
+pkill -9 .*chrome
+pkill -9 node
 
 cd /home/dalimilhajek/main-source/
 yarn start --headless | tee -a ../served/log.txt
 
 pkill -9 java
 pkill -9 chromium
+pkill -9 .*chrome
+pkill -9 node
