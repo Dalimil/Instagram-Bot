493b6a913c0b8efec40433969d60afd1d5aa3e3d
Dalimil <dalimilhajek@gmail.com>
2019-06-27 15:36:10 -0700

    Update windows launch script

--- windows-start-script.bat
+++ windows-start-script.bat
@@ -1,5 +1,6 @@
 @echo off
 
 echo Starting Instagram Bot... Redirecting output to log.txt
+powershell.exe -Command "Start-Process PowerShell -Verb RunAs 'Stop-Process -Name 2.43-x64-chromedriver -Force' "
 powershell.exe -Command "cd ~/Downloads/Instagram-Bot/ ;; yarn start --headless 2>&1 >> ~/Downloads/log.txt"
 
