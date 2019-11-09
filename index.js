29f8bcd36d6dd59f4a8fa6c2963d86ef13be7570
Dalimil <dalimilhajek@gmail.com>
2019-11-09 14:13:50 -0800

    Fix script termination

--- index.js
+++ index.js
@@ -6,59 +6,65 @@ const TerminalBot = require('./src/terminal');
 //  or simply 'yarn start', 'yarn follow', 'yarn unfollow', 'yarn experiment',
 //  'yarn start --lightweight --headless' etc.
 (async () => {
-  // Follow limit: between 100 up to max 500 per day
-  // HARD LIMIT: 40 follows per hour!
-  // Unknown unfollow limit: about 50 per hour, but Instagram will force you to
-  //  space it out in that one hour (it pretends it allows, but in fact ignores actions over limit)
-  // 7500 total following is the global MAX
-  // Max number of likes is 1.5x that amount
+  try {
+    // Follow limit: between 100 up to max 500 per day
+    // HARD LIMIT: 40 follows per hour!
+    // Unknown unfollow limit: about 50 per hour, but Instagram will force you to
+    //  space it out in that one hour (it pretends it allows, but in fact ignores actions over limit)
+    // 7500 total following is the global MAX
+    // Max number of likes is 1.5x that amount
 
-  const commandArg = process.argv[2];
-  const skipFollow = (commandArg === '--unfollow');
-  const skipUnfollow = (commandArg === '--follow');
-  const isExperimentMode = (commandArg === '--experiment');
-  const followNumberTarget = process.argv.includes('--lightweight') ? 1 : 10;
+    const commandArg = process.argv[2];
+    const skipFollow = (commandArg === '--unfollow');
+    const skipUnfollow = (commandArg === '--follow');
+    const isExperimentMode = (commandArg === '--experiment');
+    const followNumberTarget = process.argv.includes('--lightweight') ? 1 : 10;
 
-  console.log('Started at', new Date().toLocaleString());
+    console.log('Started at', new Date().toLocaleString());
 
-  // Browser (webdriver) version of the bot
-  await BrowserBot.init();
+    // Browser (webdriver) version of the bot
+    await BrowserBot.init();
 
-  if (isExperimentMode) {
-    // EXPERIMENT MODE
-    const inputData = JSON.parse(require('fs').readFileSync('./tmp.json')).data;
-    // await BrowserBot.runBrowseList(inputData);
-    const untrackedAccounts =
-       await BrowserBot.runGetUntrackedFutureUnfollowAccounts('dali_mil', inputData);
-    console.log(untrackedAccounts);
-    // await BrowserBot.runMassUnfollowFromList(inputData.slice(0, 30));
-  } else {
-    // STANDARD MODE (10-20% conversion rate)
-    if (!skipUnfollow) {
-      await BrowserBot.runMassUnfollow(15);
+    if (isExperimentMode) {
+      // EXPERIMENT MODE
+      const inputData = JSON.parse(require('fs').readFileSync('./tmp.json')).data;
+      // await BrowserBot.runBrowseList(inputData);
+      const untrackedAccounts =
+        await BrowserBot.runGetUntrackedFutureUnfollowAccounts('dali_mil', inputData);
+      console.log(untrackedAccounts);
+      // await BrowserBot.runMassUnfollowFromList(inputData.slice(0, 30));
+    } else {
+      // STANDARD MODE (10-20% conversion rate)
+      if (!skipUnfollow) {
+        await BrowserBot.runMassUnfollow(15);
+      }
+      if (!skipFollow) {
+        // 'Follow by hashtag' follows feed likers (because these are active users)
+        await BrowserBot.runMain({ hashtag: 'pnw' }, followNumberTarget);
+        // 'Follow by username' follows accounts following the given account
+        // await BrowserBot.runMain({ username: 'jordhammond' });
+      }
+      if (!skipUnfollow) {
+        await BrowserBot.runMassUnfollow(15);
+      }
+      if (!skipFollow) {
+        await BrowserBot.runMain({ hashtag: 'vancouver' }, followNumberTarget);
+      }
+      if (!skipUnfollow && !skipFollow) {
+        // Final batch
+        await BrowserBot.runMassUnfollow(15);
+      }
     }
-    if (!skipFollow) {
-      // 'Follow by hashtag' follows feed likers (because these are active users)
-      await BrowserBot.runMain({ hashtag: 'pnw' }, followNumberTarget);
-      // 'Follow by username' follows accounts following the given account
-      // await BrowserBot.runMain({ username: 'jordhammond' });
-    }
-    if (!skipUnfollow) {
-      await BrowserBot.runMassUnfollow(15);
-    }
-    if (!skipFollow) {
-      await BrowserBot.runMain({ hashtag: 'vancouver' }, followNumberTarget);
-    }
-    if (!skipUnfollow && !skipFollow) {
-      // Final batch
-      await BrowserBot.runMassUnfollow(15);
-    }
+  } catch(e) {
+    console.error('------------- Terminated with an error -----------');
+    console.error(e);
+  } finally {
+    // No matter what error is thrown, we should terminate the processes correctly
+    await BrowserBot.end();
+    console.log('Finished at', new Date().toLocaleString());
+    // Force terminate (because selenium subprocess kill is buggy)
+    setTimeout(() => {
+      process.exit();
+    }, 5000);
   }
-
-  await BrowserBot.end();
-  console.log('Finished at', new Date().toLocaleString());
-  // Force terminate (because selenium subprocess kill is buggy)
-  setTimeout(() => {
-    process.exit();
-  }, 5000);
 })();
