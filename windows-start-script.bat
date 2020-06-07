1edaa78aa9c5b57e6460d0ac0411798ec640e4ce
Dalimil Hajek <dahajek@microsoft.com>
2020-06-07 08:00:22 -0700

    New session

--- windows-start-script.bat
+++ windows-start-script.bat
@@ -4,6 +4,7 @@ echo Starting Instagram Bot... Redirecting output to log.txt
 powershell.exe -Command "Start-Process PowerShell -Verb RunAs 'Stop-Process -Name 2.43-x64-chromedriver -Force' "
 powershell.exe -Command "Start-Process PowerShell -Verb RunAs 'Stop-Process -Name Node* -Force' "
 powershell.exe -Command "Start-Process PowerShell -Verb RunAs 'Stop-Process -Name Java* -Force' "
+timeout 4 > NUL
 powershell.exe -Command "cd ~/Downloads/Instagram-Pics/ ;; yarn start --headless 2>&1 > ./served/log8.txt"
 
 
