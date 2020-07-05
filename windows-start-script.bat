7b07fa39dce8235f73167d66234ab90d6a51d70b
Dalimil Hajek <dahajek@microsoft.com>
2020-07-05 15:50:02 -0700

    Mod start scripts

--- windows-start-script.bat
+++ windows-start-script.bat
@@ -5,6 +5,6 @@ powershell.exe -Command "Start-Process PowerShell -Verb RunAs 'Stop-Process -Nam
 powershell.exe -Command "Start-Process PowerShell -Verb RunAs 'Stop-Process -Name Node* -Force' "
 powershell.exe -Command "Start-Process PowerShell -Verb RunAs 'Stop-Process -Name Java* -Force' "
 timeout 4 > NUL
-powershell.exe -Command "cd ~/Downloads/Instagram-Pics/ ;; yarn start --headless 2>&1 > ./served/log8.txt"
+powershell.exe -Command "cd ~/Downloads/Instagram-Pics/ ;; yarn unfollow --headless 2>&1 > ./served/log8.txt"
 
 
